38dc4e482d169b63d7f831030ed3dde85531b080
hasithmaddela <hasithmaddela29@gmail.com>
2024-11-24 20:00:30 +0530

    Adding Data Providers Util

--- src/test/java/Utilities/DataProviders.java
+++ src/test/java/Utilities/DataProviders.java
@@ -1,5 +1,44 @@
 package Utilities;
 
-public class DataProviders {
+import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
+
+public class DataProviders
+{
+   String filepath;
+
+   //Get All Test Data	
+   @DataProvider(name="AllTestData")
+   public String[][] getAllTestData() throws IOException
+   {
+	   ExcelUtil eu=new ExcelUtil(filepath);
+	   int rowCount=eu.getRowCount("Sheet1");
+	   int columnCount=eu.getColumnCount("Sheet1", rowCount);
+	   String data[][]=new String[rowCount][columnCount];
+	   for(int i=1;i<=rowCount;i++)
+	   {
+		   for(int c=0;c<columnCount;c++)
+		   {
+			   data[i-1][0]=eu.getCellData("Sheet", i, c);
+		   }
+	   }
+	   return data;
+   }
+	
+   //Get Single Column Data
+   
+   @DataProvider(name="SingleColumnData")
+   public String[] getSingleColumnData() throws IOException
+   {
+	   ExcelUtil eu=new ExcelUtil(filepath);
+	   int rowCount=eu.getRowCount("Sheet1");
+	   String[] data=new String[rowCount];
+	   for(int i=1;i<=rowCount;i++)
+	   {
+		   data[i-1]=eu.getCellData("Sheet1", i, 1);
+	   }
+	   return data;
+   }
+   
 }
