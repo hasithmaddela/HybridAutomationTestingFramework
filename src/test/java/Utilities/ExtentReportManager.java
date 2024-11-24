ba11d1036b2e0ff53f6ac8879af453b094942a0d
hasithmaddela <hasithmaddela29@gmail.com>
2024-11-24 20:21:00 +0530

    Adding ExtentReportManager version 1

--- src/test/java/Utilities/ExtentReportManager.java
+++ src/test/java/Utilities/ExtentReportManager.java
@@ -1,5 +1,58 @@
 package Utilities;
 
-public class ExtentReportManager {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+
+public class ExtentReportManager implements ITestListener
+{
+	public ExtentSparkReporter sparkreporter; //Handles the UI of the Report
+	public ExtentReports extent;       //Handles common info for the Report
+	public ExtentTest test;         //Creates a Test Entry and updates the Status
+
+	public  void onTestStart(ITestResult result)
+	 {
+		
+		sparkreporter=new ExtentSparkReporter(System.getProperty("user.dir")+"\\Reports\\TestReport.html");
+		sparkreporter.config().setDocumentTitle("Test Report");
+		sparkreporter.config().setTheme(Theme.DARK);
+	    
+		extent=new ExtentReports();
+		extent.setSystemInfo("Environment", "QA");
+	  }
+
+	  public void onTestSuccess(ITestResult result) 
+	  {
+	    test=extent.createTest(result.getName());
+	    test.log(Status.PASS, "Test Case Passed:"+result.getName());
+	    
+	  }
+
+	  public void onTestFailure(ITestResult result)
+	  {
+		  test=extent.createTest(result.getName());
+		  test.log(Status.FAIL, "Test Case Failed:"+result.getName());
+		  test.log(Status.INFO,"Test Case Failed cause is:"+result.getThrowable().getMessage());
+	  }
+
+	  public void onTestSkipped(ITestResult result)
+	  {
+		  test=extent.createTest(result.getName());
+		  test.log(Status.SKIP, "Test Case Skipped:"+result.getName());
+		  test.log(Status.INFO,"Test Case Skipped cause is:"+result.getThrowable().getMessage());
+	  }
+
+	
+	  public  void onFinish(ITestContext context)
+	  {
+         extent.flush();
+	  }
+	
 }
