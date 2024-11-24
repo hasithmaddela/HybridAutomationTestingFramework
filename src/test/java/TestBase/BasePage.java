614d8d53857a0347a64c5812edbda9a357c537bd
hasithmaddela <hasithmaddela29@gmail.com>
2024-11-24 20:04:00 +0530

    Adding Base Page Class

--- src/test/java/TestBase/BasePage.java
+++ src/test/java/TestBase/BasePage.java
@@ -1,5 +1,16 @@
 package TestBase;
 
-public class BasePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage 
+{
+	public WebDriver driver;
+	
+	public BasePage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
 
 }
